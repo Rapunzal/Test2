efcdde8a5b8e7a89ed25ac9f7a30a3971107e64f
Priyanka <priyankagjune@gmail.com>
2026-09-03 17:41:30 -0600

    first commit

---
+++ src/main/java/com/example/Test/controller/Ping.java
@@ -0,0 +1,13 @@
+package com.example.Test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ping {
+
+    @GetMapping(value = "/")
+    public String ping(){
+        return "pong";
+    }
+}
